20b7483866f78a2d72b2c0a7dfd6012afe502dc5
Sujal Thomas Tatipelli <42236789+sujalthomas@users.noreply.github.com>
2022-10-26 02:03:50 -0700

    Add files via upload

---
+++ src/pages/Ecommerce.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { IoIosMore } from 'react-icons/io';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
+
+import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
+import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
+import product9 from '../data/product9.jpg';
+
+const DropDown = ({ currentMode }) => (
+  <div className="w-28 border-1 border-color px-2 py-1 rounded-md">
+    <DropDownListComponent id="time" fields={{ text: 'Time', value: 'Id' }} style={{ border: 'none', color: (currentMode === 'Dark') && 'white' }} value="1" dataSource={dropdownData} popupHeight="220px" popupWidth="120px" />
+  </div>
+);
+
+const Ecommerce = () => {
+  const { currentColor, currentMode } = useStateContext();
+
+  return (
+    <div className="mt-24">
+      
+
+      <div className="flex gap-10 m-4 flex-wrap justify-center">
+        
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl w-96 md:w-760">
+          <div className="flex justify-between items-center gap-2 mb-10">
+            <p className="text-xl font-semibold">Price Overview</p>
+            <DropDown currentMode={currentMode} />
+          </div>
+          <div className="md:w-full overflow-auto">
+            <LineChart />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap justify-center">
+        <div className="md:w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+          <div className="flex justify-between">
+            <p className="text-xl font-semibold">Weekly Stats</p>
+            <button type="button" className="text-xl font-semibold text-gray-500">
+              <IoIosMore />
+            </button>
+          </div>
+
+          <div className="mt-10 ">
+            {weeklyStats.map((item) => (
+              <div key={item.title} className="flex justify-between mt-4 w-full">
+                <div className="flex gap-4">
+                  <button
+                    type="button"
+                    style={{ background: item.iconBg }}
+                    className="text-2xl hover:drop-shadow-xl text-white rounded-full p-3"
+                  >
+                    {item.icon}
+                  </button>
+                  <div>
+                    <p className="text-md font-semibold">{item.title}</p>
+                    <p className="text-sm text-gray-400">{item.desc}</p>
+                  </div>
+                </div>
+
+                <p className={`text-${item.pcColor}`}>{item.amount}</p>
+              </div>
+            ))}
+            <div className="mt-4">
+              <SparkLine currentColor={currentColor} id="area-sparkLine" height="160px" type="Area" data={SparklineAreaData} width="320" color="rgb(242, 252, 253)" />
+            </div>
+          </div>
+
+            <p className="text-gray-400 text-sm">380 Recent Transactions</p>
+          </div>
+        </div>
+        
+      </div>
+  );
+};
+
+export default Ecommerce;
